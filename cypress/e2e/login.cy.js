022a8e2a97442c4a396395104e69d47759760e61
Bruno Cavalcanti <brunoalex.cavalcanti@gmail.com>
2025-08-16 11:32:15 -0300

    Adicionando before each aos testes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,18 +1,21 @@
 describe('Teste login de usuário', () => {
-  it('Login com dados válidos com sucesso', () => {
+  beforeEach(() => {
     //Arrange
     cy.visit('http://localhost:4000/')
+  })
+
+  it('Login com dados válidos com sucesso', () => {
     
     //Act
     cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('12345')
+    cy.get('#senha').click().type('123456')
     cy.get('#login-section > .btn').click()
 
     //Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
-  it.only('Login com dados inválidos sem sucesso', () => {
+  it('Login com dados inválidos sem sucesso', () => {
     
     //Arrange
     cy.visit('http://localhost:4000/')
