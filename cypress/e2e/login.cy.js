4c38757420f83b48cbd10e310af5ce80f2a689eb
Bruno Cavalcanti <brunoalex.cavalcanti@gmail.com>
2025-09-24 13:06:55 -0300

    Alterando a origem da URL

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,7 +2,7 @@ describe('Teste login de usuário', () => {
   beforeEach(() => {
     //Arrange
     //cy.visit(Cypress.env(URL))
-    cy.visit('http://localhost:4000')
+    cy.visit('/')
     //cy.screenshot('apos-acessar-pagina')
   })
 
