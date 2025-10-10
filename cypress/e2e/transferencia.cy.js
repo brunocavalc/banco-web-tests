71890fe1f5240dfafd74693a68f50f22853d60c3
Bruno Cavalcanti <brunoalex.cavalcanti@gmail.com>
2025-10-10 18:04:49 -0300

    Implmentando comandos customizaveis para Login, Transferencias e Commons

--- cypress/e2e/transferencia.cy.js
+++ cypress/e2e/transferencia.cy.js
@@ -1,26 +1,33 @@
 describe('Tranferencias', () => {
     beforeEach(() => {
         cy.visit('/')
-        cy.fixture('credenciais').then(credenciais => {
-            cy.get('#username').click().type(credenciais.valida.usuario)
-            cy.get('#senha').click().type(credenciais.valida.senha)
-        })
-        cy.get('#login-section > .btn').click()
+        cy.fazerLoginComCredenciaisValidas()
     }) 
 
     it('Deve transferir quando informo dados e valor validos', () => {
-        cy.get('label[for="conta-origem"]').parent().as('campo-conta-origem')
-        cy.get('@campo-conta-origem').click()
-        cy.get('@campo-conta-origem').contains('Maria Oliveira').click()
+        // Act
+        // cy.selecionarOpcaoNaCombobox('conta-origem', 'João da Silva')
+        // cy.selecionarOpcaoNaCombobox('conta-destino', 'Maria Oliveira')
+        // cy.get('#valor').click().type('11')
+        // cy.contains('button', 'Transferir').click()
 
-        cy.get('label[for="conta-destino"]').parent().as('campo-conta-destino')
-        cy.get('@campo-conta-destino').click()
-        cy.get('@campo-conta-destino').contains('João da Silva').click()
+        cy.realizarTransferencia('João da Silva', 'Maria Oliveira', '11')
 
-        cy.get('#valor').click().type('11')
-        cy.contains('button', 'Transferir').click()
+        //Assert
+        cy.verificarMensagemNoToast('Transferência realizada!')
+    })
 
-        cy.get('.toast').should('have.text', 'Transferência realizada!')
+    it('Deve apresentar erro quando tentar transferir mais que 5mil sem o token', () => {
 
+        //Act
+        // cy.selecionarOpcaoNaCombobox('conta-origem', 'João da Silva')
+        // cy.selecionarOpcaoNaCombobox('conta-destino', 'Maria Oliveira')
+        // cy.get('#valor').click().type('5000.01')
+        // cy.contains('button', 'Transferir').click()
+
+        cy.realizarTransferencia('João da Silva', 'Maria Oliveira', '5000.01')
+
+        //Assert
+        cy.verificarMensagemNoToast('Autenticação necessária para transferências acima de R$5.000,00.')
     })
 });
